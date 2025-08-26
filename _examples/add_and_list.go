0bd57869feb953afa8658d095759c3c60408882f
xiaohan <bornwarm@foxmail.com>
2025-08-26 16:29:40 +0800

    Add examples for consistent hashing with weighted members

---
+++ _examples/add_and_list.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/xiaohan2004/consistent"
+	"github.com/cespare/xxhash"
+)
+
+type Member string
+
+func (m Member) String() string {
+	return string(m)
+}
+
+func (m Member) Weight() int {
+	return 1
+}
+
+type hasher struct{}
+
+func (h hasher) Sum64(data []byte) uint64 {
+	return xxhash.Sum64(data)
+}
+
+func main() {
+	members := []consistent.Member{}
+	for i := 0; i < 8; i++ {
+		member := Member(fmt.Sprintf("node%d.olricmq", i))
+		members = append(members, member)
+	}
+	cfg := consistent.Config{
+		PartitionCount:    71,
+		ReplicationFactor: 20,
+		Load:              1.25,
+		Hasher:            hasher{},
+	}
+
+	c := consistent.New(members, cfg)
+	owners := make(map[string]int)
+	for partID := 0; partID < cfg.PartitionCount; partID++ {
+		owner := c.GetPartitionOwner(partID)
+		owners[owner.String()]++
+	}
+	fmt.Println("average load:", c.AverageLoad())
+	fmt.Println("owners:", owners)
+}
---
+++ _examples/sample.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/xiaohan2004/consistent"
+	"github.com/cespare/xxhash"
+)
+
+// In your distributed system, you probably have a custom data type
+// for your cluster members. Just add a String function to implement
+// consistent.Member interface.
+type myMember struct{
+	name string
+	weight int
+}
+
+func (m myMember) String() string {
+	return m.name
+}
+
+func (m myMember) Weight() int {
+	return m.weight
+}
+
+// consistent package doesn't provide a default hashing function.
+// You should provide a proper one to distribute keys/members uniformly.
+type hasher struct{}
+
+func (h hasher) Sum64(data []byte) uint64 {
+	// you should use a proper hash function for uniformity.
+	return xxhash.Sum64(data)
+}
+
+func main() {
+	// Create a new consistent instance
+	cfg := consistent.Config{
+		PartitionCount:    7,
+		ReplicationFactor: 20,
+		Load:              1.25,
+		Hasher:            hasher{},
+	}
+	c := consistent.New(nil, cfg)
+
+	// Add some members to the consistent hash table.
+	// Add function calculates average load and distributes partitions over members
+	node1 := myMember{
+		name: "node1.olricmq.com",
+		weight: 1,
+	}
+	c.Add(node1)
+
+	node2 := myMember{
+		name: "node2.olricmq.com",
+		weight: 2,
+	}
+	c.Add(node2)
+
+	key := []byte("my-key")
+	// calculates partition id for the given key
+	// partID := hash(key) % partitionCount
+	// the partitions is already distributed among members by Add function.
+	owner := c.LocateKey(key)
+	fmt.Println(owner.String())
+	// Prints node2.olricmq.com
+}
